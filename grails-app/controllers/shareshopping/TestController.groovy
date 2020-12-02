f0935e5a47ba148a076828693b51507c2dbb293f
chizongmin <371304086@qq.com>
2020-12-02 18:17:11 +0800

    test

--- grails-app/controllers/shareshopping/TestController.groovy
+++ grails-app/controllers/shareshopping/TestController.groovy
@@ -1,6 +1,13 @@
 package shareshopping
 
+import grails.converters.JSON
+import grails.util.Environment
+
 class TestController {
 
-    def index() { }
+    def index() {
+        def environment = Environment.current.name
+        def data = [environment: environment]
+        render data as JSON
+    }
 }
