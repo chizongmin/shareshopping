d4621e62105a83e2d47c6e98939cd9253380a80c
chizongmin <371304086@qq.com>
2020-12-26 11:47:09 +0800

    test

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -27,7 +27,7 @@ appender('FILE', RollingFileAppender){
         Pattern = "%d %level %logger %thread - %msg%n"
     }
     rollingPolicy(TimeBasedRollingPolicy) {
-        FileNamePattern = "/usr/local/logs/log.%d{yyyy-MM-dd}.log"
+        FileNamePattern = "${BuildSettings.BASE_DIR}/logs/log.%d{yyyy-MM-dd}.log"
         maxHistory = 180
     }
 }
