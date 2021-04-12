d2bc1f929ea3e994cfcf6fde7e5fdcd2939e7fa4
chizongmin <371304086@qq.com>
2021-04-12 18:10:44 +0800

    test

--- grails-app/controllers/shareshopping/TestController.groovy
+++ grails-app/controllers/shareshopping/TestController.groovy
@@ -3,7 +3,6 @@ package shareshopping
 import test.CyclicBarrierExampleService
 
 class TestController extends BaseController{
-    def orderNumberService
     CyclicBarrierExampleService cyclicBarrierExampleService
     def test() {
         rv(cyclicBarrierExampleService.runTask())
